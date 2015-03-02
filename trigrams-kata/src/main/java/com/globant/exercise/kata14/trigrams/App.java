a7a94095313bea0be6a7c6d53035b22d2a5f5d74
Lucas Portillo <lucas.portillo@globant.com>
2015-03-02 15:45:19 -0300

    Added mvn-based project files

---
+++ trigrams-kata/src/main/java/com/globant/exercise/kata14/trigrams/App.java
@@ -0,0 +1,13 @@
+package com.globant.exercise.kata14.trigrams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
